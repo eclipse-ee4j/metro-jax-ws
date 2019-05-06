84992b50f070158e6b6ef85ef546e54db3bdf15b
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-05-06 12:52:27 +0200

    should return possibly noop instance instead of null
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxws-ri/rt/src/main/java/com/sun/xml/ws/server/MonitorBase.java
+++ jaxws-ri/rt/src/main/java/com/sun/xml/ws/server/MonitorBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -394,7 +394,7 @@ class RewritingMOM implements ManagedObjectManager
     @Override public void close() throws IOException    { mom.close(); }
     @Override public void setJMXRegistrationDebug(boolean x) { mom.setJMXRegistrationDebug(x); }
     @Override public boolean isManagedObject(Object x)  { return mom.isManagedObject(x); }
-    @Override public ObjectRegistrationManager getObjectRegistrationManager() { return null; }
+    @Override public ObjectRegistrationManager getObjectRegistrationManager() { return mom.getObjectRegistrationManager(); }
 }
 
 // End of file.
