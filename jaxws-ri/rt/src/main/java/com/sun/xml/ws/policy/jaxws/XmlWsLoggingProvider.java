5a8dbfae2f1f4924c08a8ef6236bdbfd37e286e6
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-09-26 21:08:23 +0200

    adopt changes in policy
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

---
+++ jaxws-ri/rt/src/main/java/com/sun/xml/ws/policy/jaxws/XmlWsLoggingProvider.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package com.sun.xml.ws.policy.jaxws;
+
+import com.sun.xml.ws.policy.spi.LoggingProvider;
+import com.sun.xml.ws.util.Constants;
+
+/**
+ *
+ * @author lukas
+ */
+public class XmlWsLoggingProvider implements LoggingProvider {
+
+    public String getLoggingSubsystemName() {
+        return Constants.LoggingDomain;
+    }
+}
