98e0e4b2e77f125beca6e7e06ac72706b0324215
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-10-15 00:21:03 +0200

    adopt jakarta naming, new vendor fixed JPMS descriptors, added missing ones update build plugins added missing OSGi header remove obsolete files update project structures
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

---
+++ jaxws-ri/transports/local/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+module com.sun.xml.ws.transport.local {
+
+    requires com.sun.xml.ws;
+
+    exports com.sun.xml.ws.transport.local;
+
+    provides com.sun.xml.ws.api.pipe.TransportTubeFactory with
+            com.sun.xml.ws.transport.local.LocalTransportFactory,
+            com.sun.xml.ws.transport.local.InVmTransportFactory;
+
+}
