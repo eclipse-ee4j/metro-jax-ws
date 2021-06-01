ea36e6c207aa06002dde9ee487f04e6b7e6ad4fb
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-06-01 19:29:35 +0200

    set up release profile for helidon integration
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxws-ri/extras/helidon-integration/java.activation/src/main/java/org/eclipse/metro/helidon/jaf/JAFWrapper.java
+++ jaxws-ri/extras/helidon-integration/java.activation/src/main/java/org/eclipse/metro/helidon/jaf/JAFWrapper.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates.
+ * Copyright 2021 Oracle Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,10 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.eclipse.metro.helidon.jaf;
 
 /**
- * JAF wrapper to provide pre-Jakarta EE artifact 
+ * Empty class.
  */
-module java.activation {
-    requires transitive jakarta.activation;
+public final class JAFWrapper {
+
+    //to satisfy javadoc tool
+    private JAFWrapper() {
+    }
+    
 }
