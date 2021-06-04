3ff59c8c86b3b4c8b178f6f5fdd857705157fa76
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-06-04 14:08:01 +0200

    #215: WsImport.doMain() not closing binding file upon subsequent invocations
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxws-ri/runtime/rt/src/main/java/com/sun/xml/ws/api/streaming/XMLStreamReaderFactory.java
+++ jaxws-ri/runtime/rt/src/main/java/com/sun/xml/ws/api/streaming/XMLStreamReaderFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -136,7 +136,9 @@ public abstract class XMLStreamReaderFactory {
             }
 
             // Otherwise, open URI
-            return get().doCreate(source.getSystemId(), new URL(source.getSystemId()).openStream(),rejectDTDs);
+            InputStream is = new URL(source.getSystemId()).openStream();
+            source.setByteStream(is);
+            return get().doCreate(source.getSystemId(), is, rejectDTDs);
         } catch (IOException e) {
             throw new XMLReaderException("stax.cantCreate",e);
         }
