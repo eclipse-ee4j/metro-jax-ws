a15a2e2fa1e68b908c18cec8cec13e66bcfeefc7
Lukas Jungmann <lukas.jungmann@oracle.com>
2023-06-08 12:52:31 +0200

    prevent NPE
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>
    (cherry picked from commit 2258b8e7c5da35a4400f9c3eae43f3d5c01aeda0)

--- jaxws-ri/runtime/rt/src/main/java/com/sun/xml/ws/util/MethodUtil.java
+++ jaxws-ri/runtime/rt/src/main/java/com/sun/xml/ws/util/MethodUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -142,6 +142,10 @@ public final class MethodUtil extends SecureClassLoader {
         }
         String path = "/".concat(name.replace('.', '/').concat(".class"));
         try (InputStream in = MethodUtil.class.getResourceAsStream(path)) {
+            if (in == null) {
+                throw new ClassNotFoundException(path);
+            }
+
             byte[] b = readAllBytes(in);
             return defineClass(name, b);
         } catch (IOException e) {
