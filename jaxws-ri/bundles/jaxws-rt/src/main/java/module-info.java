f875e1919c47d7372f7f7328ca80fb3f00d1a6ef
Lukas Jungmann <lukas.jungmann@oracle.com>
2023-10-17 00:57:06 +0200

    few more warnings
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxws-ri/bundles/jaxws-rt/src/main/java/module-info.java
+++ jaxws-ri/bundles/jaxws-rt/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -21,6 +21,7 @@
  *
  * @since 2.4.0
  */
+@SuppressWarnings({"deprecation"})
 module com.sun.xml.ws {
 
     requires java.desktop;
